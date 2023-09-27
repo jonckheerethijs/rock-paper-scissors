a99419251429880aafaf9d52b3b500e137c73646
jonckheerethijs <jonckheerethijs@gmail.com>
2023-09-27 13:54:30 +0200

    stole everything from somewhere else
    credit to michalosman

--- script.js
+++ script.js
@@ -1,76 +1,153 @@
-const choices = ["Rock", "Paper", "Scissors"];
+//game logic
+
 let playerScore = 0
 let computerScore = 0
+let roundWinner = ""
+
+function evaluate(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        roundWinner = 'tie'
+    }
+    if (
+        (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+        (playerSelection === 'Scissors' && computerSelection === 'Paper') ||
+        (playerSelection === 'Paper' && computerSelection === 'Rock')
+    ) {
+        playerScore++
+        roundWinner = 'player'
+    }
+    if (
+        (computerSelection === 'Rock' && playerSelection === 'Scissors') ||
+        (computerSelection === 'Scissors' && playerSelection === 'Paper') ||
+        (computerSelection === 'Paper' && playerSelection === 'Rock')
+    ) {
+        computerScore++
+        roundWinner = 'computer'
+    }
+    updateScoreMessage(roundWinner, playerSelection, computerSelection)
+}
+
+function getRandomChoice() {
+    let randomNumber = Math.floor(Math.random() * 3)
+    switch (randomNumber) {
+        case 0:
+            return 'Rock'
+        case 1:
+            return 'Paper'
+        case 2:
+            return 'Scissors'
+    }
+}
+
+function isGameOver() {
+    return playerScore === 5 || computerScore === 5
+}
 
 function capitalizeFirstLetter(string) {
-    string = string.toLowerCase();
-    return string.charAt(0).toUpperCase() + string.slice(1);
+    string = string.toLowerCase()
+    return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
-function getComputerChoice() {
-    return choices[Math.floor(Math.random() * choices.length)];
-}
-
-function getUserChoice() {
-    userInput = prompt("What do you want to choose?");
-    userChoice = capitalizeFirstLetter(userInput)
-    return userChoice;
-}
-
-function evaluate(computerSelection, userSelection) {
-    if (userSelection == computerSelection) {
-        return "draw"
-    }
-    switch (computerSelection) {
-        case "Rock":
-            if (userSelection == "Paper") {
-                playerScore++
-                return "user";
-            } else if (userSelection == "Scissors") {
-                computerScore++
-                return "computer";
-            } else {
-                return "draw";
-            }
-        case "Paper":
-            if (userSelection == "Scissors") {
-                playerScore++
-                return "user";
-            } else if (userSelection == "Rock") {
-                computerScore++
-                return "computer";
-            } else {
-                return "draw";
-            }
-        case "Scissors":
-            if (userSelection == "Paper") {
-                computerScore++
-                return "computer";
-            } else if (userSelection == "Rock") {
-                playerScore++
-                return "user";
-            } else {
-                return "draw";
-            }
-    }
-}
-
-function game(userChoice) {
-    let computerChoice = getComputerChoice();
-    result = evaluate(computerChoice, userChoice)
-    console.log(`Computer:  ${computerChoice}     User:  ${userChoice}`)
-    if (result == "draw") {
-        resultbox.textContent = `It was a draw`
-    } else {
-        resultbox.textContent = `The ${result} won`
-    }
-}
-
+// UI
 
 const rockbtn = document.getElementById('rock')
 const paperbtn = document.getElementById('paper')
 const scissorsbtn = document.getElementById('scissors')
-const resultbox = document.getElementById('result')
+
+
+const scoreInfo = document.getElementById('scoreInfo')
+const scoreMessage = document.getElementById('scoreMessage')
+
+const playerSign = document.getElementById('playerSign')
+const playerScorePara = document.getElementById('playerScore')
+const computerSign = document.getElementById('computerSign')
+const computerScorePara = document.getElementById('computerScore')
+
 rockbtn.addEventListener('click', () => game('Rock'))
 paperbtn.addEventListener('click', () => game('Paper'))
 scissorsbtn.addEventListener('click', () => game('Scissors'))
+
+function game(playerChoice) {
+    let computerChoice = getRandomChoice()
+    evaluate(playerChoice, computerChoice)
+    updateChoices(playerChoice, computerChoice)
+    updateScoreInfo()
+}
+
+function updateScoreInfo() {
+    if (roundWinner === 'tie') {
+      scoreInfo.textContent = "It's a tie!"
+    } else if (roundWinner === 'player') {
+      scoreInfo.textContent = 'You won!'
+    } else if (roundWinner === 'computer') {
+      scoreInfo.textContent = 'You lost!'
+    }
+  
+    playerScorePara.textContent = `Player: ${playerScore}`
+    computerScorePara.textContent = `Computer: ${computerScore}`
+  }
+
+function updateScoreMessage(winner, playerSelection, computerSelection) {
+    if (winner === 'player') {
+        scoreMessage.textContent = `${capitalizeFirstLetter(playerSelection)} 
+        beats ${computerSelection.toLowerCase()}`
+        return
+    }
+    if (winner === 'computer') {
+        scoreMessage.textContent = `${capitalizeFirstLetter(playerSelection)} 
+        is beaten by ${computerSelection.toLowerCase()}`
+        return
+    }
+
+    scoreMessage.textContent = `${capitalizeFirstLetter(
+        playerSelection
+    )} ties with ${computerSelection.toLowerCase()}`
+}
+
+function updateScoreMessage(roundWinner, playerSelection, computerSelection) {
+    if (roundWinner === 'player') {
+        scoreMessage.textContent = `${capitalizeFirstLetter(
+            playerSelection
+        )} beats ${computerSelection.toLowerCase()}`
+        return
+    }
+    if (roundWinner === 'computer') {
+        scoreMessage.textContent = `${capitalizeFirstLetter(
+            playerSelection
+        )} is beaten by ${computerSelection.toLowerCase()}`
+        return
+    }
+
+    scoreMessage.textContent = `${capitalizeFirstLetter(
+        playerSelection
+    )} ties with ${computerSelection.toLowerCase()}`
+}
+
+
+function updateChoices(computerSelection, playerSelection) {
+    switch (playerSelection) {
+        case 'Rock':
+            playerSign.textContent = '✊'
+            break
+        case 'Paper':
+            playerSign.textContent = '✋'
+            break
+        case 'Scissors':
+            playerSign.textContent = '✌'
+            break
+    }
+
+    switch (computerSelection) {
+        case 'Rock':
+            computerSign.textContent = '✊'
+            break
+        case 'Paper':
+            computerSign.textContent = '✋'
+            break
+        case 'Scissors':
+            computerSign.textContent = '✌'
+            break
+    }
+}
+
+
